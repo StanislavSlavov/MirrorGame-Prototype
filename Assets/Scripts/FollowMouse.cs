f67961fea0bf33f8bdc821396efc849b4499ce97
phantasmagogue <83072954+phantasmagogue@users.noreply.github.com>
2021-07-06 00:27:32 +0300

    Ok
    new stuff

---
+++ Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    Vector3 mouse_pos;
+    public Transform target; //Assign to the object you want to rotate
+    Vector3 object_pos;
+    float angle;
+
+    private void FixedUpdate()
+    {
+        mouse_pos = Input.mousePosition;
+        mouse_pos.z = 5.23f; //The distance between the camera and object
+        object_pos = Camera.main.WorldToScreenPoint(target.position);
+        mouse_pos.x = mouse_pos.x - object_pos.x;
+        mouse_pos.y = mouse_pos.y - object_pos.y;
+        angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+
+    public float speed;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+            {
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+
+            Vector3 tempVect = new Vector3(h, v, 0);
+            tempVect = tempVect.normalized * speed *Time.deltaTime;
+            rb.MovePosition(rb.transform.position + tempVect);
+            }
+    
+
+    }
+}
